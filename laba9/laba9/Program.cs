23c1efcaf01cac458b455a721db740df8fd99fe5
cherniayskivlad <vladchernav210@gmail.com>
2022-02-19 09:18:20 +0300

    laba9

---
+++ laba9/laba9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba9
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Findarr t1 = new Findarr(3,5);
+            t1.Negatival(3,5);
+            Console.ReadKey();
+        }
+    }
+}
