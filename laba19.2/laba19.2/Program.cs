e8e722b59e680c395659d420e51e3da9dda65587
Kalorad <60627049+VladKalorad@users.noreply.github.com>
2022-10-11 08:33:53 +0300

    laba19.2 (#31)

---
+++ laba19.2/laba19.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba19._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tovar[] arr = new Tovar[4];
+            arr[0] = new Tovar(12, 11, "a", "abc");
+            arr[1] = new Tovar(12, 11, "a", "abc");
+            arr[2] = new Tovar(13, 10, "b", "qwe");
+            arr[3] = new Tovar(14, 19, "c", "zxc");
+            var zxc = arr.DistinctBy(x => x.Name).ToList();
+            foreach (var item in zxc)
+
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ laba19.2/laba19.2/Tovar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba19._2
+{
+    class Tovar
+    {
+        public string Name;
+        public int Weight;
+        public string Category;
+        public int Price;
+        public Tovar(int weight, int price, string name, string cagory)
+        {
+            Weight = weight;
+            Price = price;
+            Name = name;
+            Category = cagory;
+        }
+        public override string ToString()
+        {
+            return $"Weight:{Weight}\nPrice:{Price}\nName:{Name}\nCategory:{Category}";
+        }
+      }
+  }
