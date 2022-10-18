a5a9c23bcb644e73e9b76cbdedf36a12adc0f1db
Kalorad <60627049+VladKalorad@users.noreply.github.com>
2022-10-18 18:54:37 +0300

    laba11.2 (#10)
    * laba11.2
    
    * laba11.2

---
+++ 4,2/4,2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_2
+{
+    class Program
+    {
+        static void inputarr(int[] arr)
+        {
+ 
+            for(int i = 0; i < arr.Length; ++i)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        static void nullarr(int[] arr)
+        {
+            Console.WriteLine("Индекс нулевых элементов массива: ");
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                if(arr[i] == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Console.WriteLine("Нулевых элементов нет");
+                }
+            }
+        }
+        static void task2(int[]arr)
+        { 
+            int index = Array.FindIndex(arr, item => item == 0);
+            for(int i = 0; i < arr.Length; ++i)
+            {
+                Console.WriteLine($"Нулевые элементы массива:{index} ", index,arr);    
+            }
+        }
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите элемента массива: ");
+            int[] arr = new int[size];
+            int[] arrtask2 = new int[] { };
+            inputarr(arr);
+            nullarr(arr);
+            task2(arr);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ laba11.1/laba11.1/ICloneable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace laba11._1
+{
+        interface ICloneable
+        {
+            object Clone();
+        }
+}
