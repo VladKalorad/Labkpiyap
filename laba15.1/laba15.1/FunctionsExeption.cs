95f87b3faa1e3e6e4f52d65b358cfe1f6f07b5d8
Kalorad <60627049+VladKalorad@users.noreply.github.com>
2022-10-21 12:13:43 +0300

    laba15 (#22)

---
+++ laba15.1/laba15.1/FunctionsExeption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba15._1
+{
+    class FunctionsExeption : Exception
+    {
+        public FunctionsExeption(string message) : base(message) { }
+    }
+}
